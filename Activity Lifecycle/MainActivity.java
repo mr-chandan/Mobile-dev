a30eec9abd482198e625e628e52be2acea3852d6
Chandan H <chandanchethan321@gmail.com>
2025-09-29 18:24:23 +0530

    Update MainActivity.java

--- Activity Lifecycle/MainActivity.java
+++ Activity Lifecycle/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,41 +12,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "onCreate called", Toast.LENGTH_SHORT).show();
+        Log.d("Activity_Lifecycle", "onCreate invoked");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Toast.makeText(this, "onStart called", Toast.LENGTH_SHORT).show();
+        Log.d("Activity_Lifecycle", "onStart invoked");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Toast.makeText(this, "onResume called", Toast.LENGTH_SHORT).show();
+        Log.d("Activity_Lifecycle", "onResume invoked");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Toast.makeText(this, "onPause called", Toast.LENGTH_SHORT).show();
+        Log.d("Activity_Lifecycle", "onPause invoked");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         Toast.makeText(this, "onStop called", Toast.LENGTH_SHORT).show();
+        Log.d("Activity_Lifecycle", "onStop invoked");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         Toast.makeText(this, "onRestart called", Toast.LENGTH_SHORT).show();
+        Log.d("Activity_Lifecycle", "onRestart invoked");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Toast.makeText(this, "onDestroy called", Toast.LENGTH_SHORT).show();
+        Log.d("Activity_Lifecycle", "onDestroy invoked");
     }
 }
+
